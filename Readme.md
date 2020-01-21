4fa69f8f3cc3d08a8f6f05bb840bb5f718864bb1
mrbing1987 <373095582@qq.com>
2020-01-21 10:04:33 +0800

    新建Readme

---
+++ Readme.md
@@ -0,0 +1,5 @@
+开发环境：Visual Studio 2019
+
+目标框架：.net framework 4.5
+
+数据库：SQL Server 2016
--- Sqlsugar.ORM.Application/Program.cs
+++ Sqlsugar.ORM.Application/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sqlsugar.ORM.Application.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,23 @@ namespace Sqlsugar.ORM.Application
     {
         static void Main(string[] args)
         {
+            // 设置SQL Server 2016数据库连接字符串
+            SqlsugarHelper.ConnectionString = string.Format("server={0};uid={1};pwd={2};database={3}", "18.216.245.248", "sa", "Mrbing4inlovr", "HoriProductQuery");
+
+            // 连接SQL Server 2016数据库
+            bool contectedResult = SqlsugarHelper.ContectedDataBase();
+            if (contectedResult)
+            {
+                // 查询全部
+                var result = SqlsugarHelper.QueryAllData<AntiCodeInfo>().ToList();
+                result = result;
+            }
+            else
+            {
+                Console.WriteLine("数据库连接失败！");
+            }
+
+            Console.ReadKey();
         }
     }
 }
